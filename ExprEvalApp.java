2267cfbec8722831b52d7e4e77f691c43679d03b
San <changsan2438@gmail.com>
2016-03-22 01:08:05 +0900

    Implemented comparison for operators in shunting-yard algorithm.

--- ExprEvalApp.java
+++ ExprEvalApp.java
@@ -8,7 +8,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 // import Java Stack Libs
-import java.util.Scanner;
 import java.util.Stack;
 
 // import Java console IO
@@ -17,93 +16,103 @@ import java.io.IOException;
 
 
 class EvalListener extends ExprBaseListener {
-   // hash-map for variables' integer value for assignment
-   Map<String, Integer> vars = new HashMap<String, Integer>(); 
+    // hash-map for variables' integer value for assignment
+    Map<String, Integer> vars = new HashMap<String, Integer>();
+    // Hash-map for operators' integer value to represent priority.
+    Map<String, Integer> opPriority = new HashMap<String, Integer>();
+    // stack for expression tree evaluation
+    Stack<Object> evalStack = new Stack<Object>();
+    // stack for operators (+-*/()) in shunting-yard algorithm
+    Stack<String> opStack = new Stack<String>();
 
-   // stack for expression tree evaluation
-   Stack<Integer> evalStack = new Stack<Integer>();
-   // stack for operators (+-*/) in shunting-yard algorithm 
-   Stack<String> opStack = new Stack<String>();
+    //Initialize Class.
+    public EvalListener() {
+        opPriority.put("+", 0);
+        opPriority.put("-", 0);
+        opPriority.put("*", 1);
+        opPriority.put("/", 1);
+    }
 
-   @Override
-   public void enterProg(ExprParser.ProgContext ctx) {
-       System.out.println("enterProg: ");
-   }
-   @Override
-   public void exitProg(ExprParser.ProgContext ctx) {
-      System.out.println("exitProg: ");
+    @Override
+    public void enterProg(ExprParser.ProgContext ctx) {
+        System.out.println("enterProg: ");
+    }
 
-   }
-   @Override
-   public void enterExpr(ExprParser.ExprContext ctx) {
-      System.out.println("enterExpr: ");
+    @Override
+    public void exitProg(ExprParser.ProgContext ctx) {
+        System.out.println("exitProg: ");
 
-   }
-   @Override
-   public void exitExpr(ExprParser.ExprContext ctx) {
-      System.out.println("exitExpr: ");
-   }
+    }
+
+    @Override
+    public void enterExpr(ExprParser.ExprContext ctx) {
+        System.out.println("enterExpr: ");
+
+    }
+
+    @Override
+    public void exitExpr(ExprParser.ExprContext ctx) {
+        System.out.println("exitExpr: ");
+    }
 
 
-   @Override
-   public void enterAssn(ExprParser.AssnContext ctx) {
-      System.out.println("enterAssn: "); 
-   }
-   @Override
-   public void exitAssn(ExprParser.AssnContext ctx) {
-      System.out.println("exitAssn: "); 
-   }
+    @Override
+    public void enterAssn(ExprParser.AssnContext ctx) {
+        System.out.println("enterAssn: ");
+    }
 
-   // Add more override methods if needed
- 
-   @Override 
-   public void visitTerminal(TerminalNode node) {
-      String s = node.getText();
+    @Override
+    public void exitAssn(ExprParser.AssnContext ctx) {
+        System.out.println("exitAssn: ");
+    }
 
-      switch(s) {
-      case "+": 
-          System.out.println("Terminal PLUS");
-          opStack.push("+");
-          break;
-      case "-": 
-          System.out.println("Terminal MINUS");
-          opStack.push("-");
-          break;
-      case "*": 
-          System.out.println("Terminal MULTIPLY");
-          opStack.push("*");
-          break;
-      case "/": 
-          System.out.println("Terminal DIVIDE");
-          opStack.push("/");
-          break;
-      case "(": 
-          System.out.println("Terminal LEFT_PAR");
-          break;
-      case ")": 
-          System.out.println("Terminal RIGHT_PAR");
-          break;
-      default:
-          if (s.matches("[0-9]+")) { // INT
-             System.out.println("Terminal-INT " + s);
-             Integer i = new Integer(s);
-             evalStack.push(i);
-          } else { // ID
-             System.out.println("Terminal-ID " + s);
-             // lookup vars-Map and push to evalStack
-             // Integer v = ....
-             // evalStack.push(v);
-              //if(vars.putIfAbsent(,))
-          } 
-      }
-   }
+    // Add more override methods if needed
+
+    @Override
+    public void visitTerminal(TerminalNode node) {
+        String s = node.getText();
+        if(s.matches("[+|-|*|/|(|)]"))  pushOpToStack(s);
+        else if (s.matches("[0-9]+")) { // INT
+            System.out.println("Terminal-INT " + s);
+            Integer i = new Integer(s);
+            //It can't distinguish value of assn from INT;
+            evalStack.push(i);
+        } else if(s.matches("[a-zA-Z]")){ // ID
+            //Now it will print even NEWLINE WS,
+            System.out.println("Terminal-ID " + s);
+            // lookup vars-Map and push to evalStack
+            // Integer v = ....
+            // evalStack.push(v);
+            //if(vars.putIfAbsent(,))
+        }
+    }
+    /*
+     * Compare operator based on Shunting-Yard Algorithm.
+     */
+    public void pushOpToStack(String op) {
+        if (opStack.empty() || op.equals("(")) {
+            opStack.push(op);
+        } else if (op.equals(")")){
+            if(opStack.empty()) {
+                System.err.println("No (");
+                return;
+            }
+            while(!opStack.lastElement().equals("(")){
+                evalStack.push(opStack.pop());
+            }
+            opStack.pop();
+        } else if (opPriority.get(op) <= opPriority.get(opStack.lastElement())){
+            evalStack.push(opStack.pop());
+            pushOpToStack(op);
+        } else
+            opStack.push(op);
+
+    }
 }
-
 public class ExprEvalApp {
    public static void main(String[] args) throws IOException {
       System.out.println("** Expression Eval w/ antlr-listener **");
 
-      //반드시 c의 역할이 어떤 것인지는 모르겠으나 그냥 실습시간에 많이 사용한 scanner를 사용할 것,
       Console c = System.console();
       if (c == null) {
          System.err.println("No Console");
