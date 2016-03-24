a1937ce6cf23abc3c4f2331d3f39bfc0a2fa1786
San <changsan2438@gmail.com>
2016-03-24 22:14:54 +0900

    Get input from file. user can pass argument or if there's no argument it print some message to tell user to put input of file path.

--- ExprEvalApp.java
+++ ExprEvalApp.java
@@ -3,7 +3,7 @@ import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 
 // import Java Map Libs
-import java.io.Reader;
+import java.io.FileInputStream;
 import java.util.*;
 
 // import Java Stack Libs
@@ -22,7 +22,7 @@ class EvalListener extends ExprBaseListener {
     Stack<Object> evalStack = new Stack<Object>();
     // stack for operators (+-*/()) in shunting-yard algorithm
     Stack<String> opStack = new Stack<String>();
-    // value for cheking in terminal node if this is from assn or expr.
+    // value for checking in terminal node if this is from assn or expr.
     boolean isAssnPropBlocked = false;
 
     //Initialize Class.
@@ -35,18 +35,18 @@ class EvalListener extends ExprBaseListener {
 
     @Override
     public void enterProg(ExprParser.ProgContext ctx) {
-        System.out.println("enterProg: ");
+        //System.out.println("enterProg: ");
     }
 
     @Override
     public void exitProg(ExprParser.ProgContext ctx) {
-        System.out.println("exitProg: ");
+       // System.out.println("exitProg: ");
 
     }
 
     @Override
     public void enterExpr(ExprParser.ExprContext ctx) {
-        System.out.println("enterExpr: ");
+       // System.out.println("enterExpr: ");
 
 
     }
@@ -58,27 +58,25 @@ class EvalListener extends ExprBaseListener {
             while(!opStack.isEmpty()){
                 evalStack.push(opStack.pop());
             }
-            System.out.println("output stack is " + evalStack.toString());
+            //System.out.println("output stack is " + evalStack.toString());
             System.out.println(calcPostFix());
         }
-        System.out.println("exitExpr: ");
+       // System.out.println("exitExpr: ");
     }
 
 
     @Override
     public void enterAssn(ExprParser.AssnContext ctx) {
-        System.out.println("enterAssn: ");
-        System.out.println(ctx.ID().getText());
-        System.out.println(ctx.INT().getText());
+       // System.out.println("enterAssn: ");
+       // System.out.println(ctx.ID().getText());
+       // System.out.println(ctx.INT().getText());
         vars.put(ctx.ID().getText(), Integer.parseInt(ctx.INT().getText()));
         isAssnPropBlocked = true;
     }
 
     @Override
     public void exitAssn(ExprParser.AssnContext ctx) {
-//        System.out.println(ctx.ID().getText());
-//        System.out.println(ctx.INT().getText());
-        System.out.println("exitAssn: ");
+      //  System.out.println("exitAssn: ");
         isAssnPropBlocked = false;
     }
 
@@ -89,15 +87,15 @@ class EvalListener extends ExprBaseListener {
         String s = node.getText();
         if(s.matches("[\\+|\\-|*|/|(|)]"))  pushOpToStack(s);
         else if (s.matches("[0-9]+")) { // INT
-            System.out.println("Terminal-INT " + s);
+            //System.out.println("Terminal-INT " + s);
             //Use Flag to check if this signal from assn or expr.
-            if(isAssnPropBlocked == false){
+            if(!isAssnPropBlocked){
                 evalStack.push(Integer.parseInt(s));
             }
         } else if(s.matches("[a-zA-Z]")){ // ID
             //Now it will print even NEWLINE WS,
-            System.out.println("Terminal-ID " + s);
-            if(isAssnPropBlocked == false){
+            //System.out.println("Terminal-ID " + s);
+            if(!isAssnPropBlocked){
                 if(vars.containsKey(s))
                     evalStack.push(vars.get(s));
                 else {
@@ -111,7 +109,7 @@ class EvalListener extends ExprBaseListener {
      * Compare operator based on Shunting-Yard Algorithm.
      */
     public void pushOpToStack(String op) {
-        System.out.println("Terminal "+op);
+        //System.out.println("Terminal "+op);
         if (opStack.empty() || op.equals("(") || opStack.lastElement().equals("(")) {
             opStack.push(op);
         } else if (op.equals(")")){
@@ -126,7 +124,7 @@ class EvalListener extends ExprBaseListener {
         }else if (opPriority.get(op) <= opPriority.get(opStack.lastElement())){
             evalStack.push(opStack.pop());
             pushOpToStack(op);
-        //새로들어오는 인자의 우선순위가 기존 인자보다 작을때.
+         //if input op' priority is lower than lastElement in stack.
         } else
             opStack.push(op);
     }
@@ -135,24 +133,25 @@ class EvalListener extends ExprBaseListener {
         if(evalStack.size() == 1) return ((Number)evalStack.remove(0)).intValue();
         for(int i=0; i < evalStack.size(); i++){
             if( evalStack.get(i) instanceof String){
-                //remove하는 순서가 바뀌면 stack의 인덱스 값에 영향을 미치므로 뒤부터 뺌.
+                //Change of removing sequence will change the results.
                 String s = (String) evalStack.remove(i);
-                int operand2 = ((Integer)evalStack.remove(i - 1)).intValue();
-                int operand1 = ((Integer)evalStack.remove(i - 2)).intValue();
+                int operand2 = (Integer) evalStack.remove(i - 1);
+                int operand1 = (Integer) evalStack.remove(i - 2);
 
                 switch (s) {
                     case "+":
-                        evalStack.add(i-2,new Integer(operand1+operand2));
+                        evalStack.add(i-2, operand1 + operand2);
                         break;
                     case "-":
-                        evalStack.add(i-2,new Integer(operand1-operand2));
+                        evalStack.add(i-2, operand1 - operand2);
                         break;
                     case "*":
-                        evalStack.add(i-2,new Integer(operand1*operand2));
+                        evalStack.add(i-2, operand1 * operand2);
                         break;
                     //문제는 실수값이 나올 수 있는데 / 연산자 때문에 몫만 구해짐.
                     case "/":
-                        evalStack.add(i-2,new Integer(operand1/operand2));
+                        if(operand1%operand2 == 0)
+                            evalStack.add(i-2, operand1 / operand2);
                         break;
                 }
                 break;
@@ -163,23 +162,29 @@ class EvalListener extends ExprBaseListener {
 }
 public class ExprEvalApp {
    public static void main(String[] args) throws IOException {
-      System.out.println("** Expression Eval w/ antlr-listener **");
-
-      Console c = System.console();
+       //file stream.
+       FileInputStream inputStream = null;
+       Console c = System.console();
       if (c == null) {
          System.err.println("No Console");
          System.exit(1);
       }
+       //! i comment this area because i noticed that it should get input from file.
        //Use Reader class as input. In the original skeleton code it uses c.readLine I need multi-line input. Instead of implementing it by
        //other methods It is possible just pass Reader class to ANTLRInputStream because it is already implemented in it.
-       Reader input = c.reader();
+       //Reader input = c.reader();
 
-       //Comment the skeleton code.
-      //String input = c.readLine("Input: ");
-      //input += '\n';
+       if(args.length == 1){
+        inputStream  = new FileInputStream(args[0]);
+       }else {
+           System.out.println("** Expression Eval w/ antlr-listener **");
+           String filePath = c.readLine("File Path Input: ");
+           inputStream = new FileInputStream(filePath);
+       }
+
 
       // Get lexer
-      ExprLexer lexer = new ExprLexer(new ANTLRInputStream(input));
+      ExprLexer lexer = new ExprLexer(new ANTLRInputStream(inputStream));
       // Get a list of matched tokens
       CommonTokenStream tokens = new CommonTokenStream(lexer);
       // Pass tokens to parser
